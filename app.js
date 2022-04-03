405daac8478dceb5a5475da5dde9b6221c690a8d
ludvigbolin <57351161+ludvigbolin@users.noreply.github.com>
2022-04-03 14:01:15 +0200

    canvas was nice

--- app.js
+++ app.js
@@ -1,64 +1,68 @@
-function changeSquare() {
-    var cell = document.getElementById(this.id);
- 		//var image = document.getElementById("image2,2");
-    // If image is currently green square, change to red, and vice versa
+function drawPoint(context, x, y, color, size) {
+    if (color == null) {
+      color = '#000';
+  }
+  if (size == null) {
+      size = 5;
+  }
 
-    if (cell.style.backgroundColor.match("white")) {
-        cell.style.backgroundColor = "#1e1e1e"
-    } else {
-        cell.style.backgroundColor = "white"
-    }
-    
-};
+    // to increase smoothing for numbers with decimal part
+    var pointX = Math.round(x);
+  var pointY = Math.round(y);
 
-function printInfo(inputImage) {
-    document.getElementById("info").innerHTML = "Info:" + inputImage + "<br /> <br /> <br />";
-};
+  context.beginPath();
+  context.fillStyle = color;
+  context.arc(pointX, pointY, size, 0 * Math.PI, 2 * Math.PI);
+  context.fill();
 
-//Creates a grid of dimensions width by height
-function makeGrid(height, width) {
-
-    // Loop over height and width to create black square objects with
-    // buttons in middle
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-
-            // Outer div is black square
-            var div = document.createElement("div");
-            div.className = "square";
-            div.id = ("cell").concat(i,",", j);
-
-            var innerDiv0 = document.createElement("div");
-            innerDiv0.className = "content";
-            div.id = ("innerDiv0").concat(i,",", j);
-            div.appendChild(innerDiv0);
-
-            // InnerDiv1 & 2 are table structures (necessary for alignment)
-            var innerDiv1 = document.createElement("div");
-            innerDiv1.className = "table";
-            div.id = ("innerDiv1").concat(i,",", j);
-            innerDiv0.appendChild(innerDiv1);
-
-            var innerDiv2 = document.createElement("div");
-            innerDiv2.className = "table-cell";
-            div.id = ("innerDiv2").concat(i,",", j);
-            innerDiv1.appendChild(innerDiv2);
-
-            document.getElementById("par").appendChild(div);
-
-            // Add onclick feature. I've tried using different methods
-            // and putting it above the appendChild line, but nothing seems
-            // to work.
-            div.onclick = changeSquare;
-        }
-    }
-};
-
-function setColor(x, y, color) {
-    var id = ("cell").concat(x,",", y);
-    var cell = document.getElementById(id);
-
-    cell.style.backgroundColor = color
 }
 
-makeGrid(100, 100);
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+var amountOfClickedPoints = 0;
+
+function drawOnClick(event) {
+    amountOfClickedPoints++;
+    var canvas = document.querySelector('#myCanvas');
+    
+    if (amountOfClickedPoints == 3)  {
+        //clearCanvas(canvas);
+        amountOfClickedPoints = 0;
+    }
+    
+    
+    var pos = getMousePos(canvas, event)
+
+    var context = canvas.getContext('2d');
+    drawPoint(context, pos.x, pos.y, 'black', 3);
+}
+
+function clearCanvas(canvas) {
+    
+    const context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function randomPoints(amountOfPoints) {
+    for (i = 0; i < amountOfPoints; i++) {
+        var x = getRandomInt(0, 501);
+        var y = getRandomInt(0, 501);
+
+        var canvas = document.querySelector('#myCanvas');
+        var context = canvas.getContext('2d');
+        drawPoint(context, x, y, 'black', 3);
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
